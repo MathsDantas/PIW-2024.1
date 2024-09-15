04faf8febc416766c5c086d3d8ff7f3da292638b
João Victor Santos <jvictorips1@gmail.com>
2024-09-15 14:51:45 -0300

    15/09

--- (obsoleto)back-end/src/database/index.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite'  // Caminho para o arquivo do banco de dados SQLite
-});
-
-export default sequelize;
