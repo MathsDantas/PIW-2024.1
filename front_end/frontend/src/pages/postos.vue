1d3b0a070b5138cda4934aeb4a939e854ca0b4ab
Matheus Dantas <matheusdantasb@outlook.com>
2024-09-16 15:49:17 -0300

    Update postos.vue

--- front_end/frontend/src/pages/postos.vue
+++ front_end/frontend/src/pages/postos.vue
@@ -9,7 +9,7 @@
   <script>
   import NavBar from '@/components/NavBar.vue';
 
-  export default {   //Caso a NavBar não apareça :( 
+  export default {   //Caso a NavBar não apareça na parte superior ok simm:( 
   name: 'Postos',
   components: {
     NavBar, // Registrando o componente NavBar
