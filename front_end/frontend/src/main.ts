2fa1a060ed0b28c089780dfa79105564e3234abe
Matheus Dantas <matheusdantasb@outlook.com>
2024-09-22 16:07:44 -0300

    Update main.ts

--- front_end/frontend/src/main.ts
+++ front_end/frontend/src/main.ts
@@ -13,20 +13,4 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 createApp(App)
 .use(router)
 .mount('#app');
-
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-
-// Criando a instância do Pinia
-const pinia = createPinia()
-
-// Criando a instância do Vue
-const app = createApp(App)
-
-// Usando o Pinia como store no aplicativo
-app.use(pinia)
-
-// Montando o app
-app.mount('#app')
+    
